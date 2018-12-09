8ab1259efdc3f82050734f3c2d0ccaeba5fc9cb7
k <xingya.shi@gmail.com>
2018-12-09 14:40:39 +0800

    add ColorField component

---
+++ src/components/fields/ColorField.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Color from 'color';
+import ChromePicker from 'react-color/lib/components/chrome/Chrome';
+
+function formatColor(color) {
+    const rgb = color.rgb;
+    return `rgba(${rgb.r},${rgb.g},${rgb.b},${rgb.a})`;
+}
+
+
+export default function ColorField(props) {
+    const [pickerOpened, setPickerOpen] = useState(false);
+    const colorInput = useRef(null);
+    const style = { backgroundColor: props.value };
+
+    const getColor = () => {
+        try {
+            return Color(props.value).rgb();
+        } catch (err) {
+            console.error('Error parsing color:', err);
+            return Color('rgb(255,255,255)');
+        }
+    };
+
+    const togglePicker = () => setPickerOpen(!pickerOpened);
+
+    const calcPickerOffset = () => {
+        const elem = colorInput.current;
+        if (elem) {
+            const pos = elem.getBoundingClientRect();
+            return {
+                top: pos.top,
+                left: pos.left + 196
+            };
+        } else {
+            return {
+                top: 160,
+                left: 555,
+            };
+        }
+    };
+
+
+    const offset = calcPickerOffset();
+    let currentColor = getColor().object();
+    currentColor = {
+        r: currentColor.r,
+        g: currentColor.g,
+        b: currentColor.b,
+        a: currentColor.alpha
+    };
+
+    const pickerStyle = {
+        position: 'fixed',
+        zIndex: 1,
+        left: offset.left,
+        top: offset.top,
+    };
+    const picker =
+        <div
+            className='maputnik-color-picker-offset'
+            style={pickerStyle}
+        >
+            <ChromePicker
+                color={currentColor}
+                onChange={c => props.onChange(formatColor(c))}
+            />
+            <div
+                className='maputnik-color-picker-offset'
+                onClick={togglePicker}
+                style={{ zIndex: -1, position: 'fixed', top: '0px', right: '0px', bottom: '0px', left: '0px' }}
+            />
+        </div>;
+
+    return (
+        <div className='maputnik-color-wrapper'>
+            {pickerOpened && picker}
+            <div className='maputnik-color-swatch' style={style} ></div>
+            <input
+                spellCheck='false'
+                className='maputnik-color'
+                ref={colorInput}
+                onClick={togglePicker}
+                style={props.style}
+                name={props.name}
+                placeholder={props.default}
+                value={props.value ? props.value : ''}
+                onChange={e => props.onChange(e.target.value)}
+            />
+        </div>
+    );
+}
+
+ColorField.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    doc: PropTypes.string,
+    style: PropTypes.object,
+    default: PropTypes.string
+};
---
+++ src/components/layers/JsonEditor.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+import InputBlock from '../input/InputBlock';
+import StringInput from '../input/StringInput';
+import jsonlint from 'jsonlint';
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/lint/lint';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/addon/lint/lint.css';
+import '../../styles/codemirror-maputnik.css';
+
+
+export default class JSONEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: JSON.stringify(props.selectedLayer, null, 2);
+        };
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        return {
+            code: JSON.stringify(props.selectedLayer, null, 2)
+        };
+    }
+
+    onCodeUpdate(newCode) {
+        try {
+            const _layers = JSON.parse(newCode);
+            this.props.onLayerChanged(_layers);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            this.setState({
+                code: newCode
+            });
+        }
+    }
+
+    resetValue() {
+        this.setState({
+            code: JSON.stringify(this.props.selectedLayer, null, 2)
+        });
+    }
+
+    render() {
+        const options = {
+            mode: { name: 'javascript', json: true },
+            tabSize: 2,
+            theme: 'maputnik',
+            viewportMargin: Infinity,
+            lineNumbers: true,
+            lint: true,
+            gutters: ['CodeMirror-lint-markers'],
+            scrollbarStyle: 'null',
+        };
+        return (
+            <CodeMirror
+                value={this.state.code}
+                options={options}
+                onBeforeChange={(editor, data, value) => this.onCodeUpdate(value)}
+                onFocusChange={focused => focused ? true : this.resetValue()}
+            />
+        );
+    }
+}
+
+JSONEditor.propTypes = {
+    selectedLayer: PropTypes.object.isRequired,
+    onLayerChanged: PropTypes.func
+};
