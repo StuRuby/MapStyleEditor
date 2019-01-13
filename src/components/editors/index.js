307e32797dccef58cac675beb94df4767288a3c3
k <xingya.shi@gmail.com>
2019-01-14 07:44:02 +0800

    update SourceTypeEditor  editor component path

---
+++ src/components/editors/index.js
@@ -0,0 +1,3 @@
+export { default as GeoJSONSourceEditor } from './GeoJSONSourceEditor';
+export { default as TileJSONSourceEditor } from './TileJSONSourceEditor';
+export { default as TileURLSourceEditor } from './TileURLSourceEditor';
