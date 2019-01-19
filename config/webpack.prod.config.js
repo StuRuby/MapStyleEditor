2e41eb427576256e585695f16b2837712ec3565b
k <xingya.shi@gmail.com>
2019-01-19 23:48:14 +0800

    add gzip webpack config

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -5,6 +5,7 @@ const commonWebpackPlugin = require('./webpack.common.config');
 const cleanWebpackPlugin = require('clean-webpack-plugin');
 const progressBarWebpackPlugin = require('progress-bar-webpack-plugin');
 const Visualizer = require('webpack-visualizer-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const cleanOptions = {
     root: path.resolve(__dirname, '../dist'),
@@ -17,13 +18,20 @@ module.exports = merge(commonWebpackPlugin, {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('prod')
+                NODE_ENV: JSON.stringify('prod')
             }
         }),
         new cleanWebpackPlugin(['*'], cleanOptions),
         new progressBarWebpackPlugin(),
         new Visualizer({
             filename: './statistics.html'
+        }),
+        new CompressionPlugin({
+            test: /\.(js|css)$/,
+            algorithm: 'gzip',
+            filename: '[path].gz[query]',
+            threshold: 8192,
+            minRatio: 0.8
         })
     ]
 });
